6e22f27fa7c0b253e7b39ebdf7f8734446de80e6
66668 <sjy0118atsn@163.com>
2018-01-13 10:17:15 +0800

    注册界面搭建

--- app/src/main/java/com/jianhua/reg/RegistActivity.java
+++ app/src/main/java/com/jianhua/reg/RegistActivity.java
@@ -2,6 +2,9 @@ package com.jianhua.reg;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jianhua.reg.utils.MLog;
 import com.jianhua.reg.utils.SPUtil;
@@ -9,10 +12,26 @@ import com.jianhua.spinner_lib.MaterialSpinner;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegistActivity extends AppCompatActivity {
+    @BindView(R.id.tv_title)
+    TextView tv_title;
+
+    @BindView(R.id.tv_right)
+    TextView tv_right;
+
     @BindView(R.id.spinner)
     MaterialSpinner spinner;
+
+    @BindView(R.id.tv_student)
+    TextView tv_student;
+
+    @BindView(R.id.tv_worker)
+    TextView tv_worker;
+
+    @BindView(R.id.img_pic)
+    ImageView img_pic;
 
     private String[] typeArrays;
     private int defaultType = 0;
@@ -23,6 +42,14 @@ public class RegistActivity extends AppCompatActivity {
         setContentView(R.layout.act_reg);
         ButterKnife.bind(this);
         initSpinner();
+        initMyView();
+    }
+
+    private void initMyView() {
+        //默认员工
+        tv_worker.setSelected(true);
+        tv_title.setText(getResources().getString(R.string.reg_title));
+        tv_right.setText(getResources().getString(R.string.reg_reset));
     }
 
     private void initSpinner() {
@@ -51,5 +78,26 @@ public class RegistActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @OnClick({R.id.tv_student, R.id.tv_worker, R.id.layout_back, R.id.btn_commit})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_student:
+                tv_student.setSelected(true);
+                tv_worker.setSelected(false);
+                break;
+            case R.id.tv_worker:
+                tv_student.setSelected(false);
+                tv_worker.setSelected(true);
+                break;
+            case R.id.layout_back:
+                this.finish();
+                break;
+            case R.id.btn_commit:
+                this.finish();
+                break;
+
+        }
     }
 }
