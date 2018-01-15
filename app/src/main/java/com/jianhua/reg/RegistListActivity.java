1b9ff74b764e7f3a43d0eaff463c351c7391459d
66668 <sjy0118atsn@163.com>
2018-01-15 14:09:20 +0800

    数据列表

--- app/src/main/java/com/jianhua/reg/RegistListActivity.java
+++ app/src/main/java/com/jianhua/reg/RegistListActivity.java
@@ -1,6 +1,14 @@
 package com.jianhua.reg;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import com.jianhua.reg.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by sjy on 2018/1/15.
@@ -8,4 +16,40 @@ import com.jianhua.reg.base.BaseActivity;
 
 public class RegistListActivity extends BaseActivity {
 
+    @BindView(R.id.radioGroup)
+    RadioGroup radioGroup;
+
+    @BindView(R.id.radioWorker)
+    RadioButton radioWorker;
+
+    @BindView(R.id.radioStudent)
+    RadioButton radioStudent;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_list);
+        ButterKnife.bind(this);
+        radioWorker.setSelected(true);
+        radioStudent.setSelected(false);
+        initListener();
+    }
+
+    private void initListener() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioWorker:
+                        radioWorker.setSelected(true);
+                        radioStudent.setSelected(false);
+                        break;
+                    case R.id.radioStudent:
+                        radioWorker.setSelected(false);
+                        radioStudent.setSelected(true);
+                        break;
+                }
+            }
+        });
+    }
 }
